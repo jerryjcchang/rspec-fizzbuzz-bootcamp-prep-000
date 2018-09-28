eccc1d7b34ca3ffaae0fceff3ca61f533650bd19
JC Chang <jerryjcchang@gmail.com>
2018-09-28 22:28:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
     return "FizzBuzz"
-  elsif int % 3 != 0 || int % 5 != 0 
-    return "nil"
   elsif int % 3 == 0
     return "Fizz"
   elsif int % 5 == 0 
     return "Buzz"
+  elsif int % 3 != 0 || int % 5 != 0 
+    return "nil"
 end
 end
